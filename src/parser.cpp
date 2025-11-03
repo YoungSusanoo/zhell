4ba647b09c3fe524fa3878441dff0aa534bc794f
YoungSusanoo <ksovfox@mail.ru>
2025-11-03 16:03:36 +0300

    Add single-line quote handling

--- src/parser.cpp
+++ src/parser.cpp
@@ -4,19 +4,45 @@
 #include <iterator>
 #include <algorithm>
 
+namespace zhell
+{
+  std::string read_quotes(std::istream& in);
+}
+
 zhell::CommandLine zhell::get_cmd(std::istream& in)
 {
   std::vector< std::string > args;
   std::string temp;
   std::getline(in, temp, '\n');
-  size_t last = 0;
-  size_t curr = temp.find(' ');
-  while (curr != std::string::npos)
+  size_t last_ws = 0;
+  for (size_t it = 0; it < temp.size(); it++)
   {
-    args.emplace_back(temp.substr(last, curr - last));
-    last = curr + 1;
-    curr = temp.find(' ', last);
+    if (temp[it] == '\"')
+    {
+      size_t last_quote = temp.find('"', it + 1);
+      if (last_quote != std::string::npos)
+      {
+        args.emplace_back(temp.substr(it + 1, last_quote - it - 1));
+        it = last_quote + 1;
+        last_ws = last_quote + 1;
+      }
+    }
+    else if (temp[it] == ' ')
+    {
+      args.emplace_back(temp.substr(last_ws, it - last_ws));
+      last_ws = it + 1;
+    }
+    else if (it + 1 == temp.size())
+    {
+      args.emplace_back(temp.substr(last_ws, temp.size() - last_ws));
+    }
   }
-  args.emplace_back(temp.substr(last, temp.size() - last));
   return CommandLine { std::move(args) };
+}
+
+std::string zhell::read_quotes(std::istream& in)
+{
+  std::string temp;
+  std::getline(in, temp, '"');
+  return temp;
 }
