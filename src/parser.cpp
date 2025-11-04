adc6fefef0df4e8340b3643f807231fe4b69251a
YoungSusanoo <ksovfox@mail.ru>
2025-11-04 15:52:24 +0300

    Add escaping handling

--- src/parser.cpp
+++ src/parser.cpp
@@ -12,31 +12,53 @@ namespace zhell
 zhell::CommandLine zhell::get_cmd(std::istream& in)
 {
   std::vector< std::string > args;
+  std::string str_line;
+  std::getline(in, str_line, '\n');
+
+  size_t token_start = 0;
+  bool escaped = false;
+  bool double_quoted = false;
   std::string temp;
-  std::getline(in, temp, '\n');
-  size_t last_ws = 0;
-  for (size_t it = 0; it < temp.size(); it++)
+  for (size_t it = 0; it < str_line.size(); it++)
   {
-    if (temp[it] == '\"')
+    if (double_quoted)
     {
-      size_t last_quote = temp.find('"', it + 1);
-      if (last_quote != std::string::npos)
+      if (str_line[it] == '"')
       {
-        args.emplace_back(temp.substr(it + 1, last_quote - it - 1));
-        it = last_quote + 1;
-        last_ws = last_quote + 1;
+        args.emplace_back(str_line.substr(token_start, it - token_start));
+        double_quoted = false;
+        token_start = it + 1;
       }
     }
-    else if (temp[it] == ' ')
+    else if (escaped)
     {
-      args.emplace_back(temp.substr(last_ws, it - last_ws));
-      last_ws = it + 1;
+      temp.append(str_line.substr(token_start, it - token_start - 1));
+      if (str_line[it] == ' ')
+      {
+        temp.append(" ");
+      }
+      if (str_line[it] == '\\')
+      {
+        temp.append("\\");
+      }
+      escaped = false;
+      token_start = it + 1;
     }
-    else if (it + 1 == temp.size())
+    else if (it != token_start && str_line[it] == ' ')
     {
-      args.emplace_back(temp.substr(last_ws, temp.size() - last_ws));
+      args.emplace_back(temp + str_line.substr(token_start, it - token_start));
+      token_start = it + 1;
+    }
+    else if (!double_quoted && str_line[it] == '\\')
+    {
+      escaped = true;
+    }
+    else if (str_line[it] == '"')
+    {
+      double_quoted = true;
     }
   }
+  args.emplace_back(temp + str_line.substr(token_start, str_line.size() - token_start));
   return CommandLine { std::move(args) };
 }
 
